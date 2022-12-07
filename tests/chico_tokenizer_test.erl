3bd007969b60b5214645b2561a2deb2785eb3acb
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2022-12-06 23:04:30 -0300

    include tests folder on rebar config

--- tests/chico_tokenizer_test.erl
+++ tests/chico_tokenizer_test.erl
@@ -3,4 +3,5 @@
 -include_lib("eunit/include/eunit.hrl").
 
 operators_test() ->
-  {ok, [{integer,1,55},{operator,1,'+'},{integer,1,52}], _} = chico_tokenizer:string("55 + 52").
+  {ok, [{integer, 1, 55}, {operator, 1, '+'}, {integer, 1, 52}], _} =
+    chico_tokenizer:string("55 + 52").
